c9da02b404376678aa062ef342f50476e41e09c2
Anitha-Paccore <147694283+Anitha-Paccore@users.noreply.github.com>
2024-06-06 19:55:39 +0530

    changes in success and failure payment

--- lib/screens/paymentfailed.dart
+++ lib/screens/paymentfailed.dart
@@ -1,23 +1,22 @@
+import 'package:architecture_pattern/screens/widgets/background_icon_widget.dart';
 import 'package:flutter/material.dart';
-
+import 'package:architecture_pattern/common_files/common_size_helper.dart';
 import '../constants/assets_constants.dart';
+import '../routes/routes_names.dart';
 
 class FailedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-
-      body: Padding(
+    return BackgroundIcon(
+      child: Padding(
         padding: const EdgeInsets.all(4.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 60,),
             Image(
               image: AssetImage(AssetConstants.paymentfailed),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: displayHeight(context) * 0.02),
             Text(
               'Payment Failed',
               style: TextStyle(
@@ -25,13 +24,15 @@ class FailedScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
-              'We are so sorry Due to <Error Message> Payment has been failed Please try again!',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                'We are so sorry. Due to <Error Message>, payment has failed. Please try again!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 15),
+              ),
             ),
-            SizedBox(height: 30),
+            SizedBox(height: displayHeight(context) * 0.01),
             Container(
               padding: EdgeInsets.all(16),
               decoration: BoxDecoration(
@@ -41,169 +42,142 @@ class FailedScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Payment Receipt',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Name',
-                          style: TextStyle(
-                            fontSize: 12,
-
-                          ),
-                        ),
-                        SizedBox(width: 127,),
-                        Text('Vinesh Kumar',  style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Transaction ID',
-                          style: TextStyle(
-                            fontSize: 12,
-
-                          ),
-                        ),
-                        SizedBox(width: 80,),
-                        Text('NEJNMK525151MKMS\n/85265/25923',
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Plan Details',
-                        style: TextStyle(
-                          fontSize: 12,
-
-                        ),
-                      ),
-                      SizedBox(width: 40,),
-                      Text('Pro Residential - \$35/Month',style: TextStyle(
-                        fontSize: 12,
+                    padding:  EdgeInsets.only(left: displayWidth(context)*0.033),
+                    child: Text(
+                      'Payment Receipt',
+                      style: TextStyle(
+                        fontSize: 18,
                         fontWeight: FontWeight.bold,
-                      ),),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Name",style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontSize: 10),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Transaction ID",style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontSize: 10.5),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Plan Details",style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontSize: 10.5),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Amount",style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontSize: 10.5),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Subscription Start Date",style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontSize: 10.5),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Renewed Automatically on",style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontSize: 10.5),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Trial Plan Expires on",style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontSize: 10.5),
+                              ),
+                            ),
+                          ],
+
+                        ),
+
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("Vinesh Kumar", style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10.5
+                              ),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("NEJNMK525151MKMS/85265/25923", style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10.1
+                              ),),
+                            ),
+                            Text("Pro Residential - \$35/Month", style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 10.5
+                            ),),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("\$0", style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10.5
+                              ),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("N/A", style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10.5
+                              ),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("N/A", style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10.5
+                              ),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text("N/A", style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10.5
+                              ),),
+                            ),
+
+                          ],
+
+                        ),
+
+                      ),
+                      // Add more widgets as needed in the Row
                     ],
                   ),
 
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Amount',
-                          style: TextStyle(
-                            fontSize: 12,
 
-                          ),
-                        ),
-                        SizedBox(width: 120,),
-                        Text('\$0',style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Subscription Start Date',
-                          style: TextStyle(
-                            fontSize: 12,
-
-                          ),
-                        ),
-                        SizedBox(width: 37,),
-                        Text('N/A',style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Renewed Automatically on',
-                          style: TextStyle(
-                            fontSize: 12,
-
-                          ),
-                        ),
-SizedBox(width: 18,),
-                        Text('N/A',style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Trial Plan Expires on',
-                          style: TextStyle(
-                            fontSize: 12,
-
-                          ),
-                        ),
-SizedBox(width: 53,),
-                        Text('N/A',style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      ],
-                    ),
-                  ),
                 ],
               ),
             ),
-            SizedBox(height: 50,),
             ElevatedButton(
-
               onPressed: () {
-
+                Navigator.pushNamed(context, RouteNames.signinScreen);
               },
+              child: Text('Login to Continue'),
+            )
 
-              child: Text('Login to Continue',
-
-              ),
-            ),
           ],
         ),
       ),
